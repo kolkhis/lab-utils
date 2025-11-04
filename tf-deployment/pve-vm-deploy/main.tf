9592148e811b15d9166a68b1cdd7f9e2e971b61f
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2025-11-04 12:24:10 -0500

    feat: Add Telmate/proxmox as required provider, format file

--- tf-deployment/pve-vm-deploy/main.tf
+++ tf-deployment/pve-vm-deploy/main.tf
@@ -1,20 +1,28 @@
+terraform {
+  required_providers {
+    terraform = {
+      source = "Telmate/proxmox"
+    }
+  }
+}
+
 provider "proxmox" {
-  pm_api_url  = var.pm_api_url
-  pm_user     = var.pm_user
-  pm_api_token_secret = var.pm_api_token_secret 
-  pm_tls_insecure = true
+  pm_api_url          = var.pm_api_url
+  pm_user             = var.pm_user
+  pm_api_token_secret = var.pm_api_token_secret
+  pm_tls_insecure     = true
 }
 
 
 resource "proxmox_vm_qemu" "test-tf-vm" {
-  name = ""
-  agent = 1
-  boot = "order=scsi0"
+  name        = ""
+  agent       = 1
+  boot        = "order=scsi0"
   target_node = "home-pve"
-  clone = "ubuntu-22.04-template"
-  storage = "vmdata"
-  cores = 2
-  memory = 2048
+  clone       = "ubuntu-22.04-template"
+  storage     = "vmdata"
+  cores       = 2
+  memory      = 2048
 }
 
 
