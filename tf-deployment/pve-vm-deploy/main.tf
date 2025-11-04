6a35a0f7334fe145420506b489622f418cc4dfc7
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2025-11-04 15:47:20 -0500

    fix: Add pm_api_token_id to provider config

--- tf-deployment/pve-vm-deploy/main.tf
+++ tf-deployment/pve-vm-deploy/main.tf
@@ -10,6 +10,7 @@ provider "proxmox" {
   pm_api_url          = var.pm_api_url
   pm_user             = var.pm_user
   pm_api_token_secret = var.pm_api_token_secret
+  pm_api_token_id     = var.pm_api_token_id
   pm_tls_insecure     = true
 }
 
