3be1af763bc68b108d8206ed39a7403cd5ba1624
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2025-11-06 19:52:58 -0500

    feat: Add base working config as example

---
+++ tf-deployment/pve-vm-deploy/cloud-init/example.tf
@@ -0,0 +1,87 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "3.0.2-rc04"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url          = var.pm_api_url
+  pm_user             = var.pm_user
+  pm_api_token_secret = var.pm_api_token_secret
+  pm_api_token_id     = var.pm_api_token_id
+  pm_tls_insecure     = true
+  pm_log_enable       = true
+  pm_log_file         = "tf-pve-plugin.log"
+  pm_debug            = true
+  pm_log_levels = {
+    _default = "debug"
+  }
+}
+
+resource "proxmox_vm_qemu" "test-tf-vm" {
+  name        = "test-rocky10-cloudinit-vm"
+  vmid        = 7000
+  agent       = 1
+  boot        = "order=scsi0"
+  target_node = "home-pve"
+  clone       = "rocky-10-cloudinit-template"
+  full_clone  = false
+
+  memory = 4096
+
+  cpu {
+    cores   = 1
+    sockets = 1
+    type    = "host"
+  }
+
+  network {
+    id     = 0
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  scsihw = "virtio-scsi-pci"
+  bios   = "ovmf"
+  efidisk {
+    storage = "vmdata"
+    efitype = "4m"
+  }
+
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          storage = "vmdata"
+          size    = "10G"
+        }
+      }
+    }
+    ide {
+      # Some images require a cloud-init disk on the IDE controller, others on the SCSI or SATA controller
+      ide2 {
+        cloudinit {
+          storage = "vmdata"
+        }
+      }
+    }
+  }
+
+  # Cloud-Init configuration
+  cicustom   = "vendor=local:snippets/qemu-guest-agent.yml" # /var/lib/vz/snippets/qemu-guest-agent.yml
+  ciupgrade  = true
+  nameserver = "1.1.1.1 8.8.8.8"
+  # ipconfig0  = "ip=192.168.4.100/24,gw=192.168.4.1,ip6=dhcp"
+  ipconfig0  = "ip=dhcp"
+  skip_ipv6  = true
+  ciuser     = var.ci_user
+  cipassword = var.ci_pass
+  sshkeys    = <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICGjGGUL4ld+JmvbDmQFu2XZrxEQio3IN7Yhgcir377t Optiplex Homelab key
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAQdazsCyvNGrXGT+zEc6l5X/JILWouFlnPchYsCeFZk kolkhis@home-pve
+EOF
+}
+
